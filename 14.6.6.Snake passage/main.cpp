113a006ed8b1c2c951a42a905c76060edb9db30a
Master Turbo <djmagg@mail.ru>
2022-11-21 09:59:17 +0300

    /

--- 14.6.6.Snake passage/main.cpp
+++ 14.6.6.Snake passage/main.cpp
@@ -3,10 +3,9 @@
 using namespace std;
 
 int main()
-{
-    int arr [5][5];
+{   int N = 5;
+    int arr [N][N];
     int number = 0;
-    int multiplier = 1;
 
     for (int i = 0; i < 5; i++)
     {
@@ -14,11 +13,8 @@ int main()
         {
             arr[i][j] = number;
             number ++;
-            cout << arr[i][j] << "\t";
         }
         cout << endl;
-        multiplier *= -1;
-        multiplier ++;
     }
 
     cout << endl;
@@ -26,18 +22,14 @@ int main()
     cout << endl;
     cout << endl;
     
-    // for (int i = 0; i < 5; i++)
-    // {
-    //     for (int j = 0; j < 5; j++)
-    //     {
-    //         cout << arr[i][j] << " ";
-    //     }
-    //     cout << endl;
-    // }
-
-
-
-
+    for (int i = 0; i < 5; i++)
+    {
+        for (int j = 0; j < 5; j++)
+        {
+            cout << arr[i][j] << " ";
+        }
+        cout << endl;
+    }
 
     return 0;
 }

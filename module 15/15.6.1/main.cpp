0e9e3b51c45cbc79c4e0219f70740aeefa82e830
Master Turbo <djmagg@mail.ru>
2022-11-22 09:23:32 +0300

    module 15 started

---
+++ module 15/15.6.1/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int sum = 0;
+    int a[] = {-2,1,-3,4,-1,2,1,-5,4};
+
+     for (int i = 0; i < 9; i++)
+     {
+        for (int j = 0; j < 9; j++)
+        {
+            sum = a[i] + a [i+x];
+        }
+        
+     }
+     
+}

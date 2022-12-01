116a4f0ea4696361475c7eecf276f6ac03e12d0e
Master Turbo <djmagg@mail.ru>
2022-12-01 05:44:54 +0300

    current state

---
+++ module 16/16.6.4/main.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+
+
+
+enum note 
+{ 
+    DO = 1, 
+    RE = 2, 
+    MI = 4, 
+    FA = 8, 
+    SOL = 16, 
+    LA = 32, 
+    SI = 64 
+};
+
+
+int main()
+{
+
+
+    return 0;
+}

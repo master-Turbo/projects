5c3d564896653a87d4a2d6bed7687a76d061a9c7
Master Turbo <djmagg@mail.ru>
2022-12-19 12:13:10 +0300

    /

--- module 18/18.5.2/main.cpp
+++ module 18/18.5.2/main.cpp
@@ -4,12 +4,12 @@ using namespace std;
 
 int counting_ways(int n)
 {
-    if (n <= 1)
-    {
-        return 1;
-    }
+    if (n < 0) return 0;
+    if (n == 0) return 1;
+
+    // cout << n << endl;
     
-    return counting_ways(n - 1) + counting_ways(n - 2) + counting_ways(n -3) ;
+    return counting_ways(n - 1) + counting_ways(n - 2) + counting_ways(n - 3);
 }
 
 int main()
@@ -17,13 +17,7 @@ int main()
     cout << "To which step should the rabbit jump?" << endl;
     int n;
     cin >> n;
-
-    // for (int i = 0; i < n; ++i)
-    // {
-    //     cout << counting_ways(i);
-    // }
-    
-    
+   
     cout << "Number of ways = " << counting_ways(n);
  
     return 0;

611ae9780b04803d7a069289bd12ecdb3703e2ec
Master Turbo <djmagg@mail.ru>
2022-12-25 10:17:37 +0300

    /

--- module 18/18.5.4/main.cpp
+++ module 18/18.5.4/main.cpp
@@ -2,22 +2,24 @@
 
 using namespace std;
 
-void qw(long long n, int ans)
+void evendigits(long long* n, int* ans)
 {
-    cout << n << " " << ans;
+    // cout << n << " " << *n;
+    // cout << *n << endl;
+    if(*n <= 0) return;
+    if((*n % 10) % 2 == 0) ++ *ans;
+    *n /= 10;
+    evendigits(n, ans);
 }
 
 
 int main()
 {
     long long n = 9223372036854775806;
-    int ans = 10;
+    int ans = 0;
 
+    evendigits(&n, &ans);
 
-
-    qw(n, ans);
-
-
-
+    cout << ans << endl;
     return 0;
 }

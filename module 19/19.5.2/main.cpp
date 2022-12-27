3f566b9acfc2d809a646f2786b0aacee5ea166dd
Master Turbo <djmagg@mail.ru>
2022-12-27 17:06:05 +0300

    /

--- module 19/19.5.2/main.cpp
+++ module 19/19.5.2/main.cpp
@@ -3,24 +3,76 @@
 
 using namespace std;
 
+enum actions
+{
+    up = 1,
+    down = 2,
+    quit = 4
+};
+
 int main()
 {
     cout << "input file path" << endl;
+    cout << "================================================" << endl;
     string path;
     getline(cin,path);
     ifstream file;
-    file.open(path);
+    file.open(path, ios::binary);
+    char buffer[64];
+    buffer[64] = 0;
     if(file.is_open())
     {
-        //code
+        file.read(buffer, sizeof (buffer));
+        cout << buffer << endl;
+        cout << "================================================" << endl;
+        cout << file.tellg() << " " << file.gcount() << endl;
+        cout << "================================================" << endl;
+        
+        while (!file.eof())
+        {
+            cout << "scroll up == 1" << endl;
+            cout << "scroll down == 2" << endl;
+            cout << "quit == 4" << endl;
+
+            int action;
+            cin >> action;
+            cout << "================================================" << endl;
+
+            switch (action)
+            {
+            case actions::up:
+                file.seekg(file.gcount(), ios::cur);//????? не перемещается назад
+                for (int i = 0; i < sizeof(buffer); ++i) buffer[i] = 0;
+                file.read(buffer, sizeof (buffer));
+                cout << buffer << endl;
+                cout << "================================================" << endl;
+                cout << file.tellg() << " " << file.gcount() << endl;
+                cout << "================================================" << endl;
+                break;
+                
+            case actions::down:
+                for (int i = 0; i < sizeof(buffer); ++i) buffer[i] = 0;
+                file.read(buffer, sizeof (buffer));
+                cout << buffer << endl;
+                cout << "================================================" << endl;
+                cout << file.tellg() << " " << file.gcount() << endl;
+                cout << "================================================" << endl;
+                break;
+                
+            case actions::quit:
+                file.close();
+                return 0;
+            
+            default:
+                break;
+            }
+        }
     }
     else
     {
         cout << "Something went wrong" << endl;
     }
     
-
-
     file.close();
     return 0;
 }

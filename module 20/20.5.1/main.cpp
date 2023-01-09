e54e5ab3ca75af374ae5e10377a7b77de645a234
Master Turbo <djmagg@mail.ru>
2023-01-09 16:46:46 +0300

    /

--- module 20/20.5.1/main.cpp
+++ module 20/20.5.1/main.cpp
@@ -3,30 +3,36 @@
 
 using namespace std;
 
+bool validation_data(string& data)
+{
+    if (stoi(data.substr(3, 2)) > 0 && stoi(data.substr(3, 2)) <= 12) return true;
+    else return false;
+}
+
+void write_file(string path)
+{
+    string data;
+    getline(cin, data);
+
+    while (data != "quit")
+    {
+        if (validation_data(data))
+        {
+            ofstream file(path, ios::app);
+            file << endl << data;
+            getline(cin, data);
+            file.close();
+        }
+        else cout << "Invalid record format" << endl;
+    }
+}
 int main()
 {
-    string data = "hello, world!";
-    ofstream file;
-    file.open("module 20/20.5.1/file.txt");
-    file << data;
-    file.close();
-
-    ifstream rdata;
-    rdata.open("module 20/20.5.1/file.txt");
-    string test;
-    rdata >> test;
-    cout << test;
-
-    rdata >> test;
-    cout << test;
-
-    rdata.close();
-
-
-
-
-
-
+    string path;
+    cout << "Enter file path" << endl;
+    cout << "To end recording, type ''quit''" << endl;
+    cin >> path;
+    write_file(path);
 
     return 0;
 }

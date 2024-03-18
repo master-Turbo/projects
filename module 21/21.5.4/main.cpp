56c0bdfb5877663ae24c73e9a61bca3d364287a7
master-Turbo <djmagg@mail.ru>
2024-03-19 01:38:35 +0300

    update

--- module 21/21.5.4/main.cpp
+++ module 21/21.5.4/main.cpp
@@ -15,54 +15,32 @@ struct player
     int coordinate_y = -1;
 };
 
-void title()
+void title() //начальная 
 {
     cout << "-------------------" << endl;
     cout << "|PLAYER vs ENEMIES|" << endl;
     cout << "-------------------" << endl << endl;
 }
 
-int init_game()
+int init_game() //выбор действия
 {
     cout << "0 > NEW GAME"<< endl;
     cout << "1 > LOAD GAME" << endl;
-    // cout << "2 > SAVE GAME" << endl;
+    cout << "2 > SAVE GAME" << endl;
 
     int init = -1;
     cin >> init;
     return init;
 }
 
-void set_coordinates_rand(player &player, vector <int> &cor_x, vector <int> &cor_y)
-{//если это не первый игрок проверить координаты уже созданных игроков
-    if (player.player_id != 0)
-    {
-        // создать рандомные координаты
-        int x = rand() % 20;
-        int y = rand() % 20;
-        // проверить не заняты ли эти координаты
-        for (int i = 0; i < player.player_id; i++)
-        {
-            if (x == cor_x[i]  && y == cor_y[i])
-            {
-                set_coordinates_rand(player, cor_x, cor_y);
-            }
-        }
-        player.coordinate_x = x;
-        player.coordinate_y = y;
-        cor_x.push_back(player.coordinate_x);
-        cor_y.push_back(player.coordinate_y);
-    }
-    else
-    {
-        player.coordinate_x = rand() % 20;
-        player.coordinate_y = rand() % 20;
-        cor_x.push_back(player.coordinate_x);
-        cor_y.push_back(player.coordinate_y);
-    }
+void set_coordinates_rand(player& player) //установить рандомные координаты
+{
+    player.coordinate_x = rand() % 20;
+    player.coordinate_y = rand() % 20;
+    // выполнить проверку на одинаковые координаты 
 }
 
-void player_constructor(player &player, vector <int> &cor_x, vector <int> &cor_y)
+void player_constructor(player& player) //конструктор игрока
 {
     cout << "name" << endl;
     cin >> player.name;
@@ -72,60 +50,56 @@ void player_constructor(player &player, vector <int> &cor_x, vector <int> &cor_y
     cin >> player.armor;
     cout << "damage" << endl;
     cin >> player.damage;
-    set_coordinates_rand(player, cor_x, cor_y);
+    set_coordinates_rand(player);
 }
 
-void enemy_constructor(player &player, int id, vector <int> &cor_x, vector <int> &cor_y)
+void enemy_constructor(player &player, int id) //конструктор врагов 
+                       
 {
     player.name = "enemy #";
     player.player_id = id;
     player.life = rand() % 100 + 50;
     player.armor = rand() % 50;
     player.damage = rand() % 15 + 15;
-    set_coordinates_rand(player, cor_x, cor_y);
+    set_coordinates_rand(player);
 }
 
-void create_matrix(vector <vector <char>> &game_matrix, const int &width, const int &height,
-                   player &player_1, player &enemy_1, player &enemy_2, player &enemy_3,
-                   player &enemy_4, player &enemy_5)
+void create_matrix(vector <vector <char>>& game_matrix, //создать заполненное точками игровое поле
+                   const int& y_cols, 
+                   const int& x_rows)
 {
-    //заполнить игровое поле точками
-    for (int i = 0; i < width; i++)
+    for (int i = 0; i < x_rows; i++)
     {
-        for (int j = 0; j < height; j++)
+        for (int j = 0; j < y_cols; j++)
         {
             game_matrix[i][j] = '.';
         }
     }
+}
 
-    //установить игроков по координатам
-    game_matrix[player_1.coordinate_x][player_1.coordinate_y] = 'P';
+void set_players(vector <vector <char>>& game_matrix, //установить игроков на игровом поле
+                 player& player_0, 
+                 player& enemy_0,
+                 player& enemy_1, 
+                 player& enemy_2,
+                 player& enemy_3, 
+                 player& enemy_4)
+{
+    game_matrix[player_0.coordinate_x][player_0.coordinate_y] = 'P';
+    game_matrix[enemy_0.coordinate_x][enemy_0.coordinate_y] = 'E';
     game_matrix[enemy_1.coordinate_x][enemy_1.coordinate_y] = 'E';
     game_matrix[enemy_2.coordinate_x][enemy_2.coordinate_y] = 'E';
     game_matrix[enemy_3.coordinate_x][enemy_3.coordinate_y] = 'E';
     game_matrix[enemy_4.coordinate_x][enemy_4.coordinate_y] = 'E';
-    game_matrix[enemy_5.coordinate_x][enemy_5.coordinate_y] = 'E';
 }
 
-void display_info(player &pl, player &e1, player &e2, player &e3, player &e4, player &e5)
+void display_matrix(vector <vector <char>>& game_matrix, //вывести на экран игровое поле
+                    const int& y_cols, 
+                    const int& x_rows)
 {
-    cout << endl;
-    cout << endl;
-    cout << "name\t\tlive\tarmor\tdamage" << endl;
-    cout << pl.name << "\t\t"<< pl.life << "\t"<< pl.armor << "\t"<< pl.damage << endl;
-    cout << e1.name << e1.player_id << "\t"<< e1.life << "\t"<< e1.armor << "\t"<< e1.damage << endl;
-    cout << e2.name << e2.player_id << "\t"<< e2.life << "\t"<< e2.armor << "\t"<< e2.damage << endl;
-    cout << e3.name << e3.player_id << "\t"<< e3.life << "\t"<< e3.armor << "\t"<< e3.damage << endl;
-    cout << e4.name << e4.player_id << "\t"<< e4.life << "\t"<< e4.armor << "\t"<< e4.damage << endl;
-    cout << e5.name << e5.player_id << "\t"<< e5.life << "\t"<< e5.armor << "\t"<< e5.damage << endl;
-
-}
-
-void display_matrix(vector <vector <char>> &game_matrix, const int &width, const int &height)
-{
-    for (int i = 0; i < width; i++)
+    for (int i = 0; i < x_rows; i++)
     {
-        for (int j = 0; j < height; j++)
+        for (int j = 0; j < y_cols; j++)
         {
             cout << game_matrix[i][j] << ' ';  
         }
@@ -133,37 +107,64 @@ void display_matrix(vector <vector <char>> &game_matrix, const int &width, const
     }
 }
 
-void display_clear()
+void display_info(player& pl, //вывести на экран информацию об игроках
+                  player& e0, 
+                  player& e1,
+                  player& e2, 
+                  player& e3, 
+                  player& e4)
+{
+    cout << endl;
+    cout << endl;
+    cout << "name\t\tlive\tarmor\tdamage" << endl;
+    cout << pl.name << "\t\t"<< pl.life << "\t"<< pl.armor << "\t"<< pl.damage << endl;
+    cout << e0.name << e0.player_id << "\t"<< e0.life << "\t"<< e0.armor << "\t"<< e0.damage << endl;
+    cout << e1.name << e1.player_id << "\t"<< e1.life << "\t"<< e1.armor << "\t"<< e1.damage << endl;
+    cout << e2.name << e2.player_id << "\t"<< e2.life << "\t"<< e2.armor << "\t"<< e2.damage << endl;
+    cout << e3.name << e3.player_id << "\t"<< e3.life << "\t"<< e3.armor << "\t"<< e3.damage << endl;
+    cout << e4.name << e4.player_id << "\t"<< e4.life << "\t"<< e4.armor << "\t"<< e4.damage << endl;
+
+}
+
+void display_clear() //функция очистки экрана
 {
     std::cout << "\033[2J\033[1;1H";
 }
 
-void move(player &player_1)
+bool border() //функция проверки на достижение игроками границы игрового поля
+{
+    bool bord = false;
+    return bord;
+}
+
+void move(player& player_0) //произвести ход игроком
 {
     cout << "your turn" << endl;
-    cout << "L > left\nR > right\nU > up\nD > down" << endl;
-    string command;
-    cin.getline(command);
+    cout << "L > left ' 'R > right ' 'U > up ' 'D > down" << endl;
+    char command;
+    cin >> command;
 
-    if (command == "L" || "l")
+    switch (command)
     {
-        player_1.coordinate_x --;
-    }
-    else if (command == "R"|| "r")
-    {
-        player_1.coordinate_x ++;
-    }
-    else if (command == "U" || "u")
-    {
-        player_1.coordinate_y --;
-    }
-    else if (command == "D" || "d")
-    {
-        player_1.coordinate_y ++;
-    }
-    else
-    {
+    case 'L':
+        player_0.coordinate_y--;
+        break;
+    
+    case 'R':
+        player_0.coordinate_y++;
+        break;
+    
+    case 'U':
+        player_0.coordinate_x--;
+        break;
+    
+    case 'D':
+        player_0.coordinate_x++;
+        break;
+
+    default:
         cout << "error command" << endl;
+        break;
     }
 }
 
@@ -171,48 +172,53 @@ void move(player &player_1)
 int main()
 {
     title();
-    if (init_game() == 0)
+
+    switch (init_game())
     {
+    case 0:
         display_clear();
-        player player_1;
-        vector <int> cor_x;
-        vector <int> cor_y;
-        player_1.player_id = 0;
-        player_constructor(player_1, cor_x, cor_y);
-        player enemy_1;
-        enemy_constructor(enemy_1, 1, cor_x, cor_y);
-        player enemy_2;
-        enemy_constructor(enemy_2, 2, cor_x, cor_y);
-        player enemy_3;
-        enemy_constructor(enemy_3, 3, cor_x, cor_y);
-        player enemy_4;
-        enemy_constructor(enemy_4, 4, cor_x, cor_y);
-        player enemy_5;
-        enemy_constructor(enemy_5, 5, cor_x, cor_y);
-        cor_x.clear();
-        cor_y.clear();
+        player player_0, enemy_0, enemy_1, enemy_2, enemy_3, enemy_4;
+        player_0.player_id = 0;
+        player_constructor(player_0);
+        enemy_constructor(enemy_0, 1);
+        enemy_constructor(enemy_1, 2);
+        enemy_constructor(enemy_2, 3);
+        enemy_constructor(enemy_3, 4);
+        enemy_constructor(enemy_4, 5);
+
         display_clear();
-        const int width = 20;
-        const int height = 20;
-        vector <vector <char>> game_matrix (width, vector <char> (height));
+        // размеры игрового поля
+        const int x_cols = 20;
+        const int y_rows = 20;
+        vector <vector <char>> game_matrix (y_rows, vector <char> (x_cols));
+
 
         while (true)
+        
         {
-            create_matrix(game_matrix, width, height, player_1, enemy_1, enemy_2, enemy_3, enemy_4, enemy_5);
-            display_matrix(game_matrix, width, height);
-            display_info(player_1, enemy_1, enemy_2, enemy_3, enemy_4, enemy_5);
-            move(player_1);
+            create_matrix(game_matrix, y_rows, x_cols);
+            set_players(game_matrix, player_0, enemy_0, enemy_1, enemy_2, enemy_3, enemy_4);
+            display_matrix(game_matrix, y_rows, x_cols);
+            display_info(player_0, enemy_0, enemy_1, enemy_2, enemy_3, enemy_4);
+            move(player_0);
             display_clear();
         }
-    }
-    if (init_game() == 1)
-    {
-        /* code */
-    }
-    if (init_game() == 2)
-    {
-        /* code */
-    }
+        
+        break;
+    
+    // case 1:
+    //     // 
+    //     break;
 
+    // case 2:
+    //     // 
+    //     break;
+    
+    // default:
+    //     cout << "error command" << endl;
+    //     break;
+    }
     return 0;
+
+
 }
